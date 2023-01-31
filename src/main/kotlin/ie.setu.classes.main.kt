6da3db843f4a1b1c7afa9c1c8e3725ffb95ad558
Wisdom Erhimwionsobo <20097898@mail.wit.ie>
2023-01-31 09:57:26 +0000

    Kotlin Syntax

--- src/main/kotlin/ie.setu.classes.main.kt
+++ src/main/kotlin/ie.setu.classes.main.kt
@@ -1,14 +1,29 @@
-package ie.setu.classes
-data class Person(var firstName: String, var lastName: String) {
+package ie.setu.classes.main.kt
+data class Person(var firstName: String, var lastName: String)
+fun main(args: Array<String>) {
 
-    fun main(args: Array<String>) {
+    val person1 = Person("Mark", "Roche")
+    val person2 = person1.copy(firstName = "Clare")
+    val person3 = person1.copy()
+    println(person1)
+    println(person2.toString())
+    println("person1 hashcode = ${person1.hashCode()}")
+    println("person2 hashcode = ${person2.hashCode()}")
+    println("person3 hashcode = ${person3.hashCode()}")
 
-        val person1 = Person("Mark", "Roche")
-        val person2 = person1.copy(firstName = "Clare")
-        println(person1)
-        println(person2.toString())
-    }
+    if (person1.equals(person2))
+        println("person1 is equal to person2.")
+    else
+        println("person1 is not equal to person2.")
+
+    if (person1.equals(person3))
+        println("person1 is equal to person3.")
+    else
+        println("person1 is not equal to person3.")
 
 }
+
+
+
 
 
